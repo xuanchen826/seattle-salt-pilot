b4e8bc194d36b00fe7e6014308df08ae84a2bc06
xuanchen826 <chenx840826@gmail.com>
2025-11-07 11:41:35 -0800

    Update seattle_salt_full_spectrum.py

--- seattle_salt_full_spectrum.py
+++ seattle_salt_full_spectrum.py
@@ -1,7 +1,7 @@
 import requests, re, pandas as pd, time, json, os
 from tqdm import tqdm
 
-API_KEY = 'AIzaSyCOEY-55qp1SArORJkXaE_l9SYLnpyubCw'
+API_KEY = 'use yours'
 TOTAL_RESTAURANTS = 150
 OUTPUT_XLSX = 'seattle_salt_full_spectrum.xlsx'
 OUTPUT_JSON = 'seattle_salt_full_spectrum_progress.json'
